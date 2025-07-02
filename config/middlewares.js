542e6793c45ec431c7700774c874c28814099d4a
Mateusz Chodacki <mateusz.chodacki@gmail.com>
2025-07-03 01:49:35 +0200

    Adding persistent storage

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,18 +1,32 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'http://gmtechnics.com', 'https://gmtechnics.com'],
+      headers: ['Content-Type', 'Authorization', 'X-Frame-Options'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:3000', 'http://gmtechnics.com', 'https://gmtechnics.com'], // Dodaj adres Reacta
-    },
-  },
 ];
